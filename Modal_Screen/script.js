d4a169f2ab99dc6183ec5fcd8f83ccb5e888615e
Raman <k.raman22222@gmail.com>
2021-02-28 23:35:51 +0530

    Added querySelector for all classes

--- Modal_Screen/script.js
+++ Modal_Screen/script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+// Here we are going to See the working of JS with the classes
+
+// We will take the use of Variables to decrease the redundant code
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+// As we have to select all the buttons consisted by the show-modal class therefore we require a deffirent selector that is queryselectorall
+const btnsShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+
+//console.log(btnsShowModal);
+// for (let i = 0; i < btnsShowModal.length; i++) {
+//   console.log(btnsShowModal[i].textContent);
+// }
