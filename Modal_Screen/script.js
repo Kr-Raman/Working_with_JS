e90afd29041654129bd93731186406b468fc6bbc
Raman <k.raman22222@gmail.com>
2021-03-01 13:00:24 +0530

    Added Esc Key Event

--- Modal_Screen/script.js
+++ Modal_Screen/script.js
@@ -25,3 +25,49 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// Now we are going to add a Escape Key event to close the modal window
+
+// we will do that by using the keyboard event listener
+// It will be defined for the entire document.
+//***************************Note************************ */
+//This keydown event generates a Object that contains all the information about the key that has been pressed which is passed to that function as a object.
+/*altKey: false
+bubbles: true
+cancelBubble: false
+cancelable: true
+charCode: 0
+code: "KeyQ"
+composed: true
+ctrlKey: false
+currentTarget: null
+defaultPrevented: false
+detail: 0
+eventPhase: 0
+isComposing: false
+isTrusted: true
+key: "q"
+keyCode: 81
+location: 0
+metaKey: false
+path: (4) [body, html, document, Window]
+repeat: false
+returnValue: true
+shiftKey: false
+sourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}
+srcElement: body
+target: body
+timeStamp: 39832.00499997474
+type: "keydown"
+view: Window {window: Window, self: Window, document: document, name: "", location: Location, …}
+which: 81
+__proto__: KeyboardEvent */
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+      console.log("esc was pressed");
+    }
+  }
+});
