4b95b33b2c1a88c1e02ed3b42d079d3d86679d97
Raman <k.raman22222@gmail.com>
2021-03-02 14:54:24 +0530

    added the New Game button

--- Dice_game/script.js
+++ Dice_game/script.js
@@ -7,12 +7,13 @@ const score0El = document.querySelector("#score--0");
 //we can select element using the ID also by the getElementID method too.
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -39,7 +40,7 @@ btnRoll.addEventListener("click", function () {
     //1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
     //2.Display Dice image
-    console.log("hello world");
+
     diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
     //3. check for rolled 1: if true then switch to next player
@@ -79,4 +80,24 @@ btnHold.addEventListener("click", function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener("click", function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  player1El.classList.remove("player--winner");
+
+  // document
+  //   .querySelector(`.player--${activePlayer}`)
+  //   .classList.remove("player--winner");
+  // document.querySelector(`.player--0`).classList.add("active-player");
 });
